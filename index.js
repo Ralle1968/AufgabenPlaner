456550e116051be079c0177194b00eb448ace940
Ralle1968 <graef.ralf@gmail.com>
2017-02-15 17:44:32 +0100

    mdl-dialogfixed

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    $(".login-cover").hide();
-  } else {
-    // No user is signed in.
-    var dialog = document.querySelector('#login-dialog');
-    if (! dialog.showModal) {
-      dialogPolyfill.registerDialog(dialog);
-    }
-    //dialog.showModal();
-  }
-});
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//     $(".login-cover").hide();
+//   } else {
+//     //No user is signed in.
+//     $("#login-dialog").hide();
+//     // var dialog = document.querySelector('#login-dialog');
+//     // if (! dialog.showModal) {
+//     //   dialogPolyfill.registerDialog(dialog);
+//     // }
+//     // dialog.showModal();
+//   }
+// });
+
+  var dialog = document.querySelector('dialog');
+    var showDialogButton = document.querySelector('#show-dialog');
+    // if (! dialog.showModal) {
+    //   dialogPolyfill.registerDialog(dialog);
+    // }
+    showDialogButton.addEventListener('click', function() {
+      dialog.showModal();
+    });
+    dialog.querySelector('.close').addEventListener('click', function() {
+      dialog.close();
+    });
