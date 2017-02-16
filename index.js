7bf17b39488cfb6563b80aeb67877d4827a4f61a
Ralle1968 <graef.ralf@gmail.com>
2017-02-16 22:37:43 +0100

    Finishing authentication

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in.
-//     $(".login-cover").hide();
-//   } else {
-//     //No user is signed in.
-//     $("#login-dialog").hide();
-//     // var dialog = document.querySelector('#login-dialog');
-//     // if (! dialog.showModal) {
-//     //   dialogPolyfill.registerDialog(dialog);
-//     // }
-//     // dialog.showModal();
-//   }
-// });
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    $(".login-cover").hide();
+    var dialog = document.querySelector('dialog');
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.close();
+  } else {
+     //No user is signed in.
+     $(".login-cover").show();
+     var dialog = document.querySelector('dialog');
+     if (! dialog.showModal) {
+       dialogPolyfill.registerDialog(dialog);
+     }
+     // dialog.showModal();
+   }
+});
 
   var dialog = document.querySelector('dialog');
-    var showDialogButton = document.querySelector('#show-dialog');
+  var showDialogButton = document.querySelector('#show-dialog');
     // if (! dialog.showModal) {
     //   dialogPolyfill.registerDialog(dialog);
     // }
-    showDialogButton.addEventListener('click', function() {
-      dialog.showModal();
+  showDialogButton.addEventListener('click', function() {
+    dialog.showModal();
+  });
+    
+  dialog.querySelector('.close').addEventListener('click', function() {
+    dialog.close();
+  });
+
+  $('#loginBtn').click(function(){
+    var email = $('#loginEmail').val();
+    var password = $('#loginPassword').val();
+    if (email !="" && password !="") {
+      $('#loginProgress').show();
+      $('#loginBtn').hide();
+      $('#closeBtn').hide();
+
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+        $('#loginError').show().text(error.message);
+        $('#loginProgress').hide();
+        $('#loginBtn').show();
+        $('#closeBtn').show();
+      });
+
+    }
+  });
+
+  $('#signOutBtn').click(function(){
+    firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    }, function(error) {
+    // An error happened.
+    alert(error.message);
     });
-    dialog.querySelector('.close').addEventListener('click', function() {
-      dialog.close();
-    });
+  });
