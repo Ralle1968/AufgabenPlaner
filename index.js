8eeaa0f3e24aae07d1d05429d648eb0fb35d348f
Ralle1968 <graef.ralf@gmail.com>
2017-02-20 23:38:23 +0100

    working on forms

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    $(".login-cover").hide();
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//     $(".login-cover").hide();
     
-    var dialog = document.querySelector('#signInAndRegister');
-    if (! dialog.showModal) {
-      dialogPolyfill.registerDialog(dialog1);
-    }
-    dialog.close();
+//     var dialog = document.querySelector('#signInAndRegister');
+//     if (! dialog.showModal) {
+//       dialogPolyfill.registerDialog(dialog1);
+//     }
+//     dialog.close();
 
-  } else {
-     //No user is signed in.
-     $(".login-cover").show();
-     var dialog = document.querySelector('#signInAndRegister');
-     if (! dialog.showModal) {
-       dialogPolyfill.registerDialog(dialog);
-     }
+//   } else {
+//      //No user is signed in.
+//      $(".login-cover").show();
+//      var dialog = document.querySelector('#signInAndRegister');
+//      if (! dialog.showModal) {
+//        dialogPolyfill.registerDialog(dialog);
+//      }
     
-     // dialog.showModal();
-   }
-});
+//      // dialog.showModal();
+//    }
+// });
 
-  var dialog = document.querySelector('#signInAndRegister');
-  var showDialogButton = document.querySelector('#show-dialog');
-    // if (! dialog.showModal) {
-    //   dialogPolyfill.registerDialog(dialog);
-    // }
-  showDialogButton.addEventListener('click', function() {
-    dialog.showModal();
-  });
+var dialog = document.querySelector('#close');
+  // var showDialogButton = document.querySelector('#show-dialog');
+  //   // if (! dialog.showModal) {
+  //   //   dialogPolyfill.registerDialog(dialog);
+  //   // }
+  // showDialogButton.addEventListener('click', function() {
+  //   dialog.showModal();
+  // });
     
-  dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
+  dialog.addEventListener('click', function() {
+    alert('Closed!');
   });
 
   // var dialog2 = document.querySelector('#register');
@@ -47,44 +47,44 @@ firebase.auth().onAuthStateChanged(function(user) {
   //   dialog2.close();
   // });
 
-  $('#loginBtn').click(function(){
-    var email = $('#loginEmail').val();
-    var password = $('#loginPassword').val();
-    if (email !="" && password !="") {
-      $('#loginProgress').show();
-      $('#loginBtn').hide();
-      $('#closeBtn').hide();
-      $('#registerBtn').hide();
+  // $('#loginBtn').click(function(){
+  //   var email = $('#loginEmail').val();
+  //   var password = $('#loginPassword').val();
+  //   if (email !="" && password !="") {
+  //     $('#loginProgress').show();
+  //     $('#loginBtn').hide();
+  //     $('#closeBtn').hide();
+  //     $('#registerBtn').hide();
 
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
-        $('#loginError').show().text(error.message);
-        $('#loginProgress').hide();
-        $('#loginBtn').show();
-        $('#closeBtn').show();
-      });
+  //     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+  //       $('#loginError').show().text(error.message);
+  //       $('#loginProgress').hide();
+  //       $('#loginBtn').show();
+  //       $('#closeBtn').show();
+  //     });
 
-    }
-  });
+  //   }
+  // });
 
-   $('#registerBtn').click(function(){
-    var email = $('#loginEmail').val();
-    var password = $('#loginPassword').val();
-    if (email !="" && password !="") {
-      $('#loginProgress').show();
-      $('#registerBtn').hide();
-      $('#loginBtn').hide();
-      $('#closeBtn').hide();
+  //  $('#registerBtn').click(function(){
+  //   var email = $('#loginEmail').val();
+  //   var password = $('#loginPassword').val();
+  //   if (email !="" && password !="") {
+  //     $('#loginProgress').show();
+  //     $('#registerBtn').hide();
+  //     $('#loginBtn').hide();
+  //     $('#closeBtn').hide();
 
-      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        $('#loginError').show().text(error.message);
-        $('#loginProgress').hide();
-        $('#registerBtn').show();
-        $('#closeBtn').show();
-      });
+  //     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  //       // Handle Errors here.
+  //       $('#loginError').show().text(error.message);
+  //       $('#loginProgress').hide();
+  //       $('#registerBtn').show();
+  //       $('#closeBtn').show();
+  //     });
 
-    }
-  });
+  //   }
+  // });
 
   $('#signOutBtn').click(function(){
     firebase.auth().signOut().then(function() {
