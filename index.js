f0e3fb366591074a897aac422cd909c3e012831a
Ralle1968 <graef.ralf@gmail.com>
2017-02-21 23:21:40 +0100

    add register logic

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
     $(".login-cover").hide();
-  
-
+    $("#cu").append(user.email);
+    
   } else {
      //No user is signed in.
      $(".login-cover").show();
-     
+     document.getElementById('signinForm').reset();
+     document.getElementById('registerForm').reset();
    }
 });
 
-// var dialog = document.querySelector('#close');
-// dialog.addEventListener('click', function() {
-//   window.location = '../start.html'
-// });
+
+var dialog = document.querySelector('#close');
+dialog.addEventListener('click', function() {
+  window.location = '../start.html'
+});
 
  
 
@@ -22,38 +24,22 @@ $('#signinBtn').click(function(){
   var email = $('#signinEmail').val();
   var password = $('#signinPass').val();
   if (email !="" && password !="") {
-    $('#loginBtn').hide();
-    $('#closeBtn').hide();
-    $('#registerBtn').hide();
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
       $('#loginError').show().text(error.message);
-      $('#loginProgress').hide();
-      $('#loginBtn').show();
-      $('#closeBtn').show();
     });
-
   }
 });
 
-  //  $('#registerBtn').click(function(){
-  //   var email = $('#registerEmail').val();
-  //   var password = $('#registerPassword').val();
-  //   if (email !="" && password !="") {
-  //     $('#loginProgress').show();
-  //     $('#registerBtn').hide();
-  //     $('#loginBtn').hide();
-  //     $('#closeBtn').hide();
-
-  //     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  //       // Handle Errors here.
-  //       $('#loginError').show().text(error.message);
-  //       $('#loginProgress').hide();
-  //       $('#registerBtn').show();
-  //       $('#closeBtn').show();
-  //     });
-
-  //   }
-  // });
+ $('#registerBtn').click(function(){
+  var email = $('#registerEmail').val();
+  var password = $('#registerPass').val();
+  //var name = $('#userName').val();
+  if (email !="" && password !="") {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      $('#loginError').show().text(error.message);
+    });
+  }
+});
 
   $('#signOutBtn').click(function(){
     firebase.auth().signOut().then(function() {
