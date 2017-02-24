25d84f2a30769b6ceb43324227d3918fc55e1b58
Ralle1968 <graef.ralf@gmail.com>
2017-02-24 22:41:55 +0100

    enable displayName

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    var firebaseRef = firebase.database().ref();
-
-    firebaseRef.child("User: " + user.uid).set({
-    name:"Rallekopp"
-    });
-
+    alert(user.email);
+    alert(user.uid);
+    alert(user.displayName);
     $(".login-cover").hide();
-    $("#cu").append(user.email);
+    $("#cu").append(user.displayName);
     
   } else {
      //No user is signed in.
      $(".login-cover").show();
+     //$("#cu").hide();
      document.getElementById('signinForm').reset();
      document.getElementById('registerForm').reset();
    }
@@ -39,17 +37,38 @@ $('#signinBtn').click(function(){
  $('#registerBtn').click(function(){
   var email = $('#registerEmail').val();
   var password = $('#registerPass').val();
-  //var name = $('#userName').val();
-  if (email !="" && password !="") {
+  var name = $('#registerName').val();
+  
+  if (email !="" && password !="" && name !="") {
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
       $('#loginError').show().text(error.message);
     });
+    }
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+    // User is signed in.
+    user.updateProfile({
+      displayName: name,
+      photoURL: 'http://www.verdi-fes.de'
+    }).then(function() {
+      // Update successful.
+      alert("Neu jetzt: " + user['displayName']);
+      $("#cu").append(user.displayName);
+    }, function(error) {
+      // An error happened.
+    });
+  } else {
+    // No user is signed in.
+
   }
+});
+     
 });
 
   $('#signOutBtn').click(function(){
     firebase.auth().signOut().then(function() {
     alert("click!");
+    window.location = '../start.html';
     }, function(error) {
     // An error happened.
     alert(error.message);
